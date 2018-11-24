8097fb061e1ec6c34cae007427dbdf089080bf42
Rysbai <rysbai.muslim@gmail.com>
2018-11-24 11:39:19 +0600

    deleted payment

--- payment/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^process/$', views.paymentProcess, name='process'),
-    url(r'^done/$', views.paymentDone, name='done'),
-    url(r'canceled/$', views.panymentCanceled, name='canceled'),
-]
