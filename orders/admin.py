15989a372d9f3b23e1ea805d42051ab3010efbdd
Rysbai <rysbai.muslim@gmail.com>
2018-11-25 15:45:47 +0600

    I finished 9th part of tutorial!

--- orders/admin.py
+++ orders/admin.py
@@ -1,5 +1,37 @@
 from django.contrib import admin
+from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.utils.html import format_html
+import csv
+import datetime
 from .models import Order, OrderItem
+
+def OrderDetail(obj):
+    return format_html('<a href="{}">Посмотреть</a>'.format(
+        reverse("orders:AdminOrderDetail", args=[obj.id])
+        ))
+def ExportToCSV(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; \
+            filename=Orders-{}.csv'.format(datetime.datetime.now().strftime("%d/%m/%Y"))
+    writer = csv.writer(response)
+
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    #Первая строка - огловления
+    writer.writerow(field.verbose_name for field in fields)
+    #Заполняем информацией
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+    ExportToCSV.short_description = 'Export CSV'
+
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
@@ -7,8 +39,9 @@ class OrderItemInline(admin.TabularInline):
 
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'addres',
-                    'postal_code', 'city', 'paid', 'created', 'updated']
+                    'postal_code', 'city', 'paid', 'created', 'updated', OrderDetail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [ExportToCSV]
 
 admin.site.register(Order, OrderAdmin)
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import OrderItem
+from django.shortcuts import render, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import OrderCreated
@@ -24,3 +25,8 @@ def orderCreate(request):
     form = OrderCreateForm()
     return render(request, 'orders/order/create.html', {'cart': cart,
                                                 'form': form})
+
+@staff_member_required
+def AdminOrderDetail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'admin/orders/order/detail.html', {'order': order})
